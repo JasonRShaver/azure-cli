ce893c2d3ef7533d404a2fd2fecb5b488bf2fdfb
Jason R. Shaver <jasonrshaver@gmail.com>
2016-11-09 01:02:20 -0800

    simple support for one level

--- src/command_modules/azure-cli-docbuilder/azure/cli/command_modules/docbuilder/custom.py
+++ src/command_modules/azure-cli-docbuilder/azure/cli/command_modules/docbuilder/custom.py
@@ -8,74 +8,68 @@ from __future__ import print_function
 from azure.cli.core.help_files import helps #pylint: disable=unused-import
 from azure.cli.core.commands import cli_command #pylint: disable=unused-import
 from azure.cli.core.application import Configuration
+from azure.cli.core._help import ArgumentGroupRegistry
+from azure.cli.core.parser import AzCliCommandParser
 
-#from azure.cli.core.application import APPLICATION, Application
 import os
 import azure.cli.core.application as application
+import azure.cli.core._help as _help
+import azure.cli.core.commands as commands
 
-def dump_help():
-    '''How to deploy an ARM template using Azure CLI.'''
-    print("""
-***********************
-ARM Template Deployment
-***********************
-
-Could this be helpful?  Let us know!
-====================================
-
-1. First Step
-2. Second Step
-
-And you're done!
-""")
-
-from azure.cli.core._help import ArgumentGroupRegistry
-#import azure.cli.core._help as helpcore
 
 def generate_review_sheet(root_group, exclude_prompts=False):
-    #cmd_table = APPLICATION.configuration.get_command_table()
-    #print(cmd_table)
-    cmd_set_names = root_group
 
-    config = Configuration([root_group])
-    application.APPLICATION = application.Application(config)
-    cmd_table = config.get_command_table()
-    cmd_list = sorted([cmd_name for cmd_name in cmd_table.keys() if cmd_set_names is None or cmd_name.split()[0] in cmd_set_names])
+    config = Configuration([])
+    cmd_table = commands.get_command_table()
+    cmd_list = cmd_table.keys()
+    filtered_cmd_listed = [cmd_name for cmd_name in cmd_table.keys() if root_group is None or cmd_name.split()[0] in root_group]
 
-    #cmd_list = cmd_table.keys()
+    my_application = application.Application(config)
+    my_application.parser.load_command_table(cmd_table)
 
-    for cmd in cmd_list:
+    for cmd in filtered_cmd_listed:
         print('\n================================================================================')
         print('=== CURRENT HELP === $ az {} -h'.format(cmd))
         print('================================================================================')
 
         cmd_string = 'az {} -h'.format(cmd)
         os.system(cmd_string)
-
+        
         print('\n********************************************************************************')
    
-        #whateverthisis = [p.group_name for p in cmd_table[cmd].arguments if p.group_name]
-        group_registry = ArgumentGroupRegistry(root_group)
+        # need to do this recursively and not just one level
+        my_subparser = my_application.parser._get_subparser(cmd.split())
+        this_parser = my_subparser.choices[cmd.split()[1]]
+        help_file = _help.CommandHelpFile(' '.join(cmd.split()), this_parser)
+        help_file.load(this_parser)
 
-        #cmd_name = [x for x in cmd_table.keys() if cmd == x][0]
-        for arg in cmd_table[cmd].arguments:
-            this_arg = cmd_table[cmd].arguments[arg]
-            print('\n++++++++++ {} + {} ++++++++++'.format(cmd, this_arg.name))
+        print('*** {}:short: {}'.format(cmd, help_file.short_summary or None))
+        print('*** Change to : ')
+        print('***')
+        print('*** {}:long: {}'.format(cmd, help_file.long_summary or None))
+        print('*** Change to : ')
+        print('***')
+        print('*** {}:examples: {}'.format(cmd, help_file.examples or None))
+        print('*** Change to : ')
+        print('***')
 
+        for thisparam in help_file.parameters:
+            if (thisparam.group_name == 'Global Arguments'):
+                continue
+            if (thisparam.group_name == 'Resource Id Arguments'):
+                continue
 
-            print(cmd_table[cmd].arguments[arg])
-            #arghelp = cmd_table[cmd].arguments[arg].name
-            print('+++ Currently + {}'.format(arghelp))
-            print('+++ Change to + ')
+            print ('*** {}:{}:short: {}'.format(cmd, thisparam.name, thisparam.short_summary or None))
+            print('*** Change to : ')
+            print('***')
+            print ('*** {}:{}:long: {}'.format(cmd, thisparam.name, thisparam.long_summary or None))
+            print('*** Change to : ')
+            print('***')
 
-        print('\n********************************************************************************', flush=True) #pylint: disable=line-too-long
-
-    print("DONE")
+        print('********************************************************************************', flush=True) #pylint: disable=line-too-long
 
 def import_review_sheet():
-    print("""
-test
-""")
+    print("Not implemented")
 
 
 cli_command('docbuilder review-sheet generate', generate_review_sheet)
